7685c81fd1f77e73123448e9b3616813b185016f
Danilo <danloose14@gmail.com>
2022-01-25 13:33:28 -0300

    criação das rotas de home e ask, criação dos partials

--- index.js
+++ index.js
@@ -2,42 +2,14 @@ const express = require('express');
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    var nome = "Danilo Loose";
-    var lang = "Javascript";
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Digibee"
-    });
+    res.render("index");
 });
 
-
-app.get("/:nome/:lang", (req, res) => {
-
-    var produtos = [
-        {
-            nome: "doritos",
-            preco: 12.00
-        }, {
-            nome: "ruffles",
-            preco: 8.00
-        }, {
-            nome: "fandangos",
-            preco: 4.00
-        }
-    ]
-
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = true;
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        produtos, produtos,
-        exibirMsg: exibirMsg
-    });
+app.get("/ask", (req, res) => {
+    res.render("ask");
 });
 
 app.listen(8080, () => {
