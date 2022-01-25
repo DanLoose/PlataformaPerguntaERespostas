18ec1d3ff803770a08c968997f66b0843289ef71
Danilo <danloose14@gmail.com>
2022-01-24 22:45:20 -0300

    utilizando variaveis e funções no HTML com ejs

--- index.js
+++ index.js
@@ -13,6 +13,33 @@ app.get("/", (req, res) => {
     });
 });
 
+
+app.get("/:nome/:lang", (req, res) => {
+
+    var produtos = [
+        {
+            nome: "doritos",
+            preco: 12.00
+        }, {
+            nome: "ruffles",
+            preco: 8.00
+        }, {
+            nome: "fandangos",
+            preco: 4.00
+        }
+    ]
+
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = true;
+    res.render("index", {
+        nome: nome,
+        lang: lang,
+        produtos, produtos,
+        exibirMsg: exibirMsg
+    });
+});
+
 app.listen(8080, () => {
     console.log("server online...");
 });
