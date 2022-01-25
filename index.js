12bd59f82ec0831c33cab091c4eaae8b65a021a2
Danilo <danloose14@gmail.com>
2022-01-25 14:02:13 -0300

    body-parser adicionado, rota para salvar no banco de dados iniciada

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-const express = require('express');
+const express = require("express");
+const body_parser = require("body-parser");
 const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+//BODY-PARSER
+app.use(body_parser.urlencoded({
+    extended: false
+}));
+app.use(body_parser.json());
+
 app.get("/", (req, res) => {
     res.render("index");
 });
 
+
+//ROUTES
 app.get("/ask", (req, res) => {
     res.render("ask");
 });
 
+app.post("/save-question", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send(`titulo: ${titulo} descrição: ${descricao}`);
+});
+
+
+//PORT
 app.listen(8080, () => {
     console.log("server online...");
 });
